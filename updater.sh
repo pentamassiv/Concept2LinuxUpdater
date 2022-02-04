064462b9626164777f315667f12d419606c66d49
pentamassiv <pentamassiv@posteo.de>
2022-02-04 01:59:28 +0100

    Update script when new firmware is available

--- updater.sh
+++ updater.sh
@@ -1,16 +1,12 @@
+cat > new_script.sh <<EOF
 #!/bin/bash
 
-UNZIP="/usr/local/bin/7z"
-JQ="/usr/bin/jq"
+DESTINATION=\$HOME/Downloads/Concept2/Firmware
 
-DESTINATION=$HOME/Downloads/Concept2/Firmware
+mkdir -p \$DESTINATION
+cd \$DESTINATION
 
-mkdir -p $DESTINATION
-cd $DESTINATION
+FILES=\$($FILES)
 
-TOKEN=
-FILES=$(curl -s "https://tech.concept2.com/api/firmware/latest" -H "Authorization: Basic ""$TOKEN" | $JQ -r '.data[] | select(.status == "public") | .files[0].name' | paste -s -d, -)
-
-curl -s -O "https://firmware.concept2.com/files/{$FILES}"
-
-$UNZIP e -y "*.7z" > /dev/null
+curl -s -O "https://firmware.concept2.com/files/{\$FILES}"
+EOF
